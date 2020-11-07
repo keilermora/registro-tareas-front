a5bad81dbcc4d21382b549d2d3f2987051c7640e
Keiler <keilermora@gmail.com>
2020-11-06 22:08:56 -0300

    agrega controles para crear, editar y eliminar tareas

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -2,57 +2,96 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Inicio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      cargando: true,
+      tareas: []
+    };
+
+    this.onClickDeleteButton = this.onClickDeleteButton.bind(this);
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    fetch("http://localhost:9000/api/tareas")
+      .then(res => res.json())
+      .then(result => {
+          this.setState({
+            cargando: false,
+            tareas: result,
+          });
+        }, error => {
+          this.setState({
+            cargando: false,
+            error: error,
+          });
+        });
+  }
+
+  onClickDeleteButton(event, identificador) {
+    fetch(`http://localhost:9000/api/tareas/${identificador}`, {
+      method: 'DELETE',
+    }).then(result => {
+      this.getData();
+    }, error => {
+      this.setState({
+        cargando: false,
+        error: error,
+      });
+    });
+  }
+
   render() {
-    return (
-      <section>
+    const { error, cargando, tareas } = this.state;
+    if (error) {
+      return <div>Ocurrió un error: {error.message}</div>;
+    } else if(cargando) {
+      return <div>Cargando...</div>;
+    } else {
+      return (
+        <section>
         <h1>Registro de tareas</h1>
         <Link to={'tarea/0'}>
           Nueva tarea
         </Link>
         <table>
-          <tr>
-            <th>Identificador</th>
-            <th>Descripcion</th>
-            <th>Vigencia</th>
-            <th>Fecha Creación</th>
-            <th></th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Tarea 1</td>
-            <td>Si</td>
-            <td>{new Date().getTime()}</td>
-            <td>
-              <Link to={'tarea/' + 1}>
-                Editar
-              </Link>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Tarea 2</td>
-            <td>Si</td>
-            <td>{new Date().getTime()}</td>
-            <td>
-              <Link to={'tarea/' + 2}>
-                Editar
-              </Link>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Tarea 3</td>
-            <td>Si</td>
-            <td>{new Date().getTime()}</td>
-            <td>
-              <Link to={'tarea/' + 3}>
-                Editar
-              </Link>
-            </td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Identificador</th>
+              <th>Descripcion</th>
+              <th>Vigencia</th>
+              <th>Fecha Creación</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {tareas.map(tarea => (
+              <tr key={tarea.identificador}>
+                <td>{tarea.identificador}</td>
+                <td>{tarea.descripcion}</td>
+                <td>{tarea.vigente}</td>
+                <td>{tarea.fechaCreacion}</td>
+                <td>
+                  <Link to={'tarea/' + tarea.identificador}>
+                    Editar
+                  </Link>
+                  <button onClick={e => this.onClickDeleteButton(e, tarea.identificador)}>
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      </section>
-    );
+        </section>
+      );
+    }
   }
 }
 
--- src/pages/Tarea/Tarea.jsx
+++ src/pages/Tarea/Tarea.jsx
@@ -4,30 +4,112 @@ import { Link } from 'react-router-dom';
 class Tarea extends Component {
   constructor(props) {
     super(props);
-    this.identificador = this.props.match.params.identificador;
+    this.state = {
+      cargando: false,
+      error: null,
+      identificador: parseInt(this.props.match.params.identificador),
+      descripcion: '',
+      vigente: false,
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if(this.state.identificador) {
+      this.setState({ cargando: true });
+      fetch(`http://localhost:9000/api/tareas/${this.state.identificador}`)
+        .then(res => res.json())
+        .then(result => {
+            this.setState({
+              cargando: false,
+              descripcion: result.descripcion,
+              vigente: result.vigente,
+            });
+          }, error => {
+            this.setState({
+              cargando: false,
+              error: error,
+            });
+          });
+    }
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { identificador, descripcion, vigente } = this.state
+    let fetchUrl = 'http://localhost:9000/api/tareas';
+    let method;
+
+    if(identificador) {
+      fetchUrl += `/${identificador}`
+      method = 'PUT';
+    } else {
+      method = 'POST';
+    }
+
+    this.setState({ cargando: true });
+
+    fetch(fetchUrl, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        descripcion: descripcion,
+        vigente: vigente,
+      })
+    })
+      .then(res => res.json())
+      .then(result => {
+          this.props.history.push('/');
+        }, error => {
+          this.setState({
+            cargando: false,
+            error: error,
+          });
+        });
   }
 
   render() {
-    return (
-      <section>
-        <h1>
-          {this.identificador === '0' ? 'Nueva tarea' : 'Editar tarea N° ' + this.identificador}
-        </h1>
-        <Link to="/">
-          Cancelar
-        </Link>
+    const { error, cargando, identificador, descripcion, vigente } = this.state;
 
-        <form action="">
-          <label htmlFor="descripcion">Descripción</label>
-          <input id="descripcion" type="text"/>
+    if(error) {
+      return <div>Ocurrió un error: {error.message}</div>;
+    } else if(cargando) {
+      return <div>Cargando...</div>;
+    } else {
+      return (
+        <section>
+          <h1>
+            {!identificador ? 'Nueva tarea' : `Editar tarea N°${identificador}`}
+          </h1>
+          <Link to="/">
+            Cancelar
+          </Link>
 
-          <input type="checkbox" name="vigente" id="vigente"/>
-          <label htmlFor="vigente">¿Vigente?</label>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="descripcion">Descripción</label>
+            <input id="descripcion" name="descripcion" type="text" value={descripcion} onChange={this.handleInputChange} required minLength={1} maxLength={255}/>
 
-          <button>Guardar</button>
-        </form>
-      </section>
-    );
+            <input type="checkbox" name="vigente" id="vigente" checked={vigente} onChange={this.handleInputChange}/>
+            <label htmlFor="vigente">¿Vigente?</label>
+
+            <button type="submit">Guardar</button>
+          </form>
+        </section>
+      );
+    }
   }
 }
 
